f0dcad071ab72aff9fc64705649704372324d83c
ifat <i>
2024-08-13 12:41:28 +0100

    nested route and navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 import Home from "./components/Home/Home";
 import Root from "./components/Root/Root";
 import "./index.css";
@@ -8,10 +10,15 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
       },
     ],
   },

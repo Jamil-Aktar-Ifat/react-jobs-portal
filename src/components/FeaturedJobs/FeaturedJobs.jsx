0271f3f2eabfcd5e354b106bd384ad32b9f12545
ifat <i>
2024-08-13 14:07:49 +0100

    page layout and display jobs features added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -18,7 +18,7 @@ const FeaturedJobs = () => {
         Explore thousands of job opportunities with all the information you
         need. Its your future
       </p>
-      <div>
+      <div className="grid grid-cols-2 gap-6 my-10">
         {jobs.map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
